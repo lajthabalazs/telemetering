bd5fa6e4e555f2a6293023e0e8410652e53fed44
Lajtha András Balázs <lajthabalazs@yahoo.com>
2014-06-08 10:46:43 +0200

    Updated tests.

--- remote_home_manager/src/test/TestDatastore.java
+++ remote_home_manager/src/test/TestDatastore.java
@@ -3,11 +3,12 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-import hu.droidium.remote_home_manager.Datastore;
+import hu.droidium.remote_home_manager.SQLJetDatastore;
 import hu.droidium.remote_home_manager.Measurement;
 
 import org.junit.After;
@@ -27,68 +28,49 @@ public class TestDatastore {
 	
 	@After
 	public void destroy() {
-		/*
 		File file = new File(TEST_DATASTORE_FILE);
 		if (file.exists()) {
 			file.delete();
 		}
-		*/
 	}
 	
 	@Test
 	public void testDatastoreInit() {
-		new Datastore(TEST_DATASTORE_FILE);
+		new SQLJetDatastore(TEST_DATASTORE_FILE);
 		assertTrue(new File(TEST_DATASTORE_FILE).exists());
 	}
 
 	@Test
 	public void testDatastoreWrite() {
-		Datastore datastore = new Datastore(TEST_DATASTORE_FILE);
-		boolean save = datastore.saveMeasuremet("temp", 35);
+		SQLJetDatastore datastore = new SQLJetDatastore(TEST_DATASTORE_FILE);
+		boolean save = datastore.saveMeasurement("temp", 123241231l, 35);
 		assertTrue(save);
 	}
 
 	@Test
 	public void testGetMeasurements() {
-		Datastore datastore = new Datastore(TEST_DATASTORE_FILE);
+		SQLJetDatastore datastore = new SQLJetDatastore(TEST_DATASTORE_FILE);
 		long startTime = System.currentTimeMillis();
 		Random random = new Random();
-		List<Long> values = new LinkedList<Long>();
-		for (int i = 0; i < 100; i++) {
-			long value = random.nextInt(30);
-			datastore.saveMeasuremet("temp", value);
-			values.add(value);
+		List<Measurement> toInsert = new LinkedList<Measurement>();
+		long time = startTime;
+		for (int i = 0; i < 100000; i++) {
+			toInsert.add( new Measurement("temp", time, (long)random.nextInt(30)));
+			time += 50;
 		}
-		long endTime = System.currentTimeMillis();
-		System.out.println("Data added");
-		List<Measurement> result = datastore.getMeasurements("temp", startTime - 1, endTime + 1);
-		assertEquals(100, result.size());
-		for (int i = 0; i < values.size(); i++) {
-			assertEquals(values.get(i), result.get(i).getValue());
-			assertEquals("temp", result.get(i).getSensorId());
-		}
-	}
-
-	
-	@Test
-	public void testFilterMeasurements() {
-		Datastore datastore = new Datastore(TEST_DATASTORE_FILE);
-		long startTime = System.currentTimeMillis();
-		Random random = new Random();
-		List<Long> values = new LinkedList<Long>();
-		for (int i = 0; i < 100; i++) {
-			long value = random.nextInt(30);
-			datastore.saveMeasuremet("temp", value);
-			values.add(value);
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		datastore.bulkInster(toInsert);
+		List<Measurement> result = datastore.getMeasurements("temp", startTime - 1, time + 1);
+		assertEquals(toInsert.size(), result.size());
+		Iterator<Measurement> toInsertIterator = toInsert.iterator();
+		int i = 0;
+		for (Measurement m : result) {
+			if (i++ % 1000 == 0) {
+				System.out.println("Testing " + i);
 			}
+			Measurement inserted = toInsertIterator.next();
+			assertEquals(inserted.getSensorId(), m.getSensorId());
+			assertEquals(inserted.getValue(), m.getValue());
+			assertEquals(inserted.getTime(), m.getTime());
 		}
-		long endTime = System.currentTimeMillis();
-		List<Measurement> result = datastore.getMeasurements("temp", startTime - 1, endTime + 1);
-		assertEquals(100, result.size());
 	}
-
 }
